85c69653ddc2dd2976e5df450dc8629b62c0b645
KassioVenicios <kassio_venicios@hotmail.com>
2018-01-31 12:37:18 -0300

    Pequenas alterações.

--- assets/js/botao.js
+++ assets/js/botao.js
@@ -1,14 +1,16 @@
 jQuery(document).ready(function() {
-            jQuery(window).scroll(function() {
-                if (jQuery(this).scrollTop() > 200) {
-                    jQuery('.Topo').fadeIn(200);
-                } else {
-                    jQuery('.Topo').fadeOut(200);
-                }
-            });
-            
-            jQuery('.Topo').click(function(event) {
-                event.preventDefault();
-                jQuery('html, body').animate({scrollTop: 0}, 300);
-            })
-        });
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 200) {
+            jQuery('.Topo').fadeIn(200);
+            jQuery('.blind').fadeIn(200);
+        } else {
+            jQuery('.Topo').fadeOut(200);
+            jQuery('.blind').fadeOut(200);
+        }
+    });
+    
+    jQuery('.Topo').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, 300);
+    })
+});
